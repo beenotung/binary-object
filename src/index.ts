3d4b11fb311fee281a7fe6096648b9e682e4302a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-06-17 13:20:32 +0800

    resolve name conflict in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export { decode, encode, clearPool } from './binary'
+export * from './binary'
+export * from './binary-file'
+export {
+  BinaryObjectSink,
+  BinaryObjectSource,
+  End,
+  Types as BinaryObjectTypes,
+} from './binary-object'
+export * from './file'
+export * from './number'
+export * from './pipe'
+export { SchemaSink, SchemaSource, Types as SchemaTypes } from './schema'
+export * from './schema-file'
