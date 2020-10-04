6616efd601f3199468ab2caaad42a3fb34ded0d4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-10-04 15:00:57 +0800

    finished impl of simple cache pipe

--- src/cache.ts
+++ src/cache.ts
@@ -1,20 +1,23 @@
-import { Sink } from './pipe'
-import { int_to_str } from './utils/base-62'
+import { Sink, Source } from './pipe'
+import { int_to_str, str_to_int } from './utils/base-62'
+
 const WORDS = {
   cache: 'c', // first seen a value what will be cached
   key: 'k', // ref to pre-cached value
   value: 'v', // non-cached value
 }
+
 export class CacheSink<T = any> extends Sink<T> {
   shouldCache: (value: T) => boolean
   encode: (value: T) => string
   values = new Map<T, number>()
+
   constructor(
+    public sink: Sink<string>,
     options: {
       shouldCache: (value: T) => boolean
       encode?: (value: T) => string
     },
-    public sink: Sink<string>,
   ) {
     super()
     this.shouldCache = options.shouldCache
@@ -38,5 +41,62 @@ export class CacheSink<T = any> extends Sink<T> {
 
   close() {
     this.sink.close()
+  }
+}
+
+function decode<T>(
+  context: {
+    decode: (string: string) => T
+    values: T[]
+  },
+  line: string,
+): T {
+  const word = line[0]
+  const string = line.substring(1)
+  switch (word) {
+    case WORDS.value:
+      return context.decode(string)
+    case WORDS.key: {
+      const key = str_to_int(string)
+      return context.values[key]
+    }
+    case WORDS.cache: {
+      const data = context.decode(string)
+      context.values.push(data)
+      return data
+    }
+    default:
+      console.error({ word, data: string })
+      throw new Error('unknown word')
+  }
+}
+
+export class CacheSource<T = any> extends Source<T> {
+  decode: (string: string) => T
+  values: T[] = []
+
+  constructor(
+    public source: Source<string>,
+    options?: {
+      decode?: (string: string) => T
+    },
+  ) {
+    super()
+    this.decode = options?.decode || (string => JSON.parse(string))
+  }
+
+  close() {
+    this.source.close()
+  }
+
+  read(): T {
+    const line = this.source.read()
+    return decode(this, line)
+  }
+
+  *iterator(options?: { autoClose?: boolean }): Generator<T> {
+    for (const line of this.source.iterator(options)) {
+      yield decode(this, line)
+    }
   }
 }
--- test/cache.test.ts
+++ test/cache.test.ts
@@ -1,5 +1,5 @@
-import { UniqueValueSource } from '../src'
-import { CacheSink } from '../src/cache'
+
+import { CacheSink, CacheSource } from '../src/cache'
 import { RawLineFileSink, RawLineFileSource } from '../src/raw-line-file'
 import { jsonSample } from './sample-object'
 import { testSuit } from './test-utils'
@@ -9,10 +9,9 @@ describe('Cache Pipe TestSuit', () => {
   testSuit(
     jsonSample,
     () =>
-      new CacheSink(
-        { shouldCache: () => true },
-        RawLineFileSink.fromFile(file),
-      ),
-    () => new UniqueValueSource(RawLineFileSource.fromFile(file)),
+      new CacheSink(RawLineFileSink.fromFile(file), {
+        shouldCache: () => true,
+      }),
+    () => new CacheSource(RawLineFileSource.fromFile(file)),
   )
 })
